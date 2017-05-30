5b8979ac844242162cd3963653db7cd9a573e739
David Smith <eomerdws@gmail.com>
2017-05-30 16:49:07 +1000

    Fixed O2DDialogTest commented out test case.

--- overlap2d/test/java/com/commons/O2DDialogTest.java
+++ overlap2d/test/java/com/commons/O2DDialogTest.java
@@ -9,6 +9,7 @@ import com.kotcrab.vis.ui.widget.VisTextField;
 import com.kotcrab.vis.ui.widget.VisValidatableTextField;
 import com.runner.LibgdxRunner;
 import com.runner.NeedGL;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,40 +29,42 @@ public class O2DDialogTest {
 
     @Before
     public void setUp() throws Exception {
-        //VisUI.load(Gdx.files.local("overlap2d/assets/style/uiskin.json"));
-        //o2DDialog = new O2DDialog("O2DDialog Test");
+        if(!VisUI.isLoaded()) {
+            VisUI.load(Gdx.files.local("overlap2d/assets/style/uiskin.json"));
+        }
+        o2DDialog = new O2DDialog("O2DDialog Test");
     }
 
     @Test
     @NeedGL
     public void shouldCreateTextFieldWithGivenText() throws Exception {
-        /*VisTextField textField = o2DDialog.createTextField("givenText");
+        VisTextField textField = o2DDialog.createTextField("givenText");
 
         assertThat(textField, not(nullValue()));
         assertThat(textField.getText(), is("givenText"));
-        assertThat(textField.getListeners().size, greaterThan(0));*/
+        assertThat(textField.getListeners().size, greaterThan(0));
     }
 
     @Test
     @NeedGL
     public void shouldCreateVisValidatableTextFieldWithInputValidator() throws Exception {
-        /*VisValidatableTextField visValidatableTextField = o2DDialog.createValidableTextField("inputText", input -> true);
+        VisValidatableTextField visValidatableTextField = o2DDialog.createValidableTextField("inputText", input -> true);
 
         assertThat(visValidatableTextField, not(nullValue()));
         assertThat(visValidatableTextField.getText(), is("inputText"));
-        assertThat(visValidatableTextField.getListeners().size, greaterThan(0));*/
+        assertThat(visValidatableTextField.getListeners().size, greaterThan(0));
     }
 
     @Test
     @NeedGL
     public void shouldAddCloseButton() throws Exception {
-        /*o2DDialog.addCloseButton();
+        o2DDialog.addCloseButton();
 
         Table titleTable = o2DDialog.getTitleTable();
         assertThat(titleTable.getCells().size, is(2));
         assertThat(titleTable.getCells(), hasItems(
                 hasProperty("actor", instanceOf(Label.class)),
                 hasProperty("actor", instanceOf(VisImageButton.class))));
-         */
+
     }
 }
